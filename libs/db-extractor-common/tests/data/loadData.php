705897b97d07a9a9d7c73aa5e7d868e4e03bd8d3
Miroslav Cillik <miro@keboola.com>
2016-02-15 16:59:45 +0100

    fix: travis allow local_infile

--- libs/db-extractor-common/tests/data/loadData.php
+++ libs/db-extractor-common/tests/data/loadData.php
@@ -8,6 +8,9 @@
 
 $pdo = new \PDO("mysql:host=localhost;dbname=test;charset=utf8", 'travis');
 
+$pdo->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+$pdo->setAttribute(\PDO::MYSQL_ATTR_LOCAL_INFILE, true);
+
 $result = $pdo->exec("LOAD DATA LOCAL
     INFILE 'escaping.csv'
     INTO TABLE escaping
