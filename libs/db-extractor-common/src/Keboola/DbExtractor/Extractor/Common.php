6076d4b39e7ed57e2817201e8a737a4217542157
Miroslav Cillik <miro@keboola.com>
2016-02-16 12:47:37 +0100

    fix: empty password

--- libs/db-extractor-common/src/Keboola/DbExtractor/Extractor/Common.php
+++ libs/db-extractor-common/src/Keboola/DbExtractor/Extractor/Common.php
@@ -26,6 +26,7 @@ class Common extends Extractor
             }
         }
 
+        $params['password'] = empty($params['password'])?null:$params['password'];
         $port = isset($params['port']) ? $params['port'] : '3306';
         $dsn = sprintf("mysql:host=%s;port=%s;dbname=%s;charset=utf8", $params['host'], $port, $params['database']);
 
