d06479096be1617e684c83d921dffa8ad1da3dc3
pivnicek <marc@keboola.com>
2016-08-04 13:16:42 +0200

    test environment settings

---
+++ apps/db-extractor-redshift/set-env.template.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export REDSHIFT_DB_HOST=my.redshift.host.region.amazonaws.com
+export REDSHIFT_DB_PORT=5439
+export REDSHIFT_DB_DATABASE=testdb
+export REDSHIFT_DB_USER=testuser
+export REDSHIFT_DB_PASSWORD=testpassword
+export AWS_ACCESS_KEY=aws_access_key
+export AWS_SECRET_KEY=aws_secret_key
+export AWS_REGION=eu-west-1
+export AWS_S3_BUCKET=test-bucket
