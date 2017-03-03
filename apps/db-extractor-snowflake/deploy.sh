0f1f7748aa595d909aa69cae43644b9cc080bf69
Erik Zigo <erik.zigo@keboola.com>
2017-03-03 13:02:26 +0100

    deploy docker image with latest tag

--- apps/db-extractor-snowflake/deploy.sh
+++ apps/db-extractor-snowflake/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag keboola/db-extractor-snowflake quay.io/keboola/db-extractor-snowflake:$TRAVIS_TAG
+docker tag keboola/db-extractor-snowflake quay.io/keboola/db-extractor-snowflake:latest
 docker push quay.io/keboola/db-extractor-snowflake:$TRAVIS_TAG
+docker push quay.io/keboola/db-extractor-snowflake:latest
