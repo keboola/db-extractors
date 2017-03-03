71b6bee4126d5ceaad22e37559af911a07481078
Erik Zigo <erik.zigo@keboola.com>
2017-03-03 01:09:02 +0100

    build on travis

---
+++ apps/db-extractor-snowflake/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker tag keboola/db-extractor-snowflake quay.io/keboola/db-extractor-snowflake:$TRAVIS_TAG
+docker push quay.io/keboola/db-extractor-snowflake:$TRAVIS_TAG
