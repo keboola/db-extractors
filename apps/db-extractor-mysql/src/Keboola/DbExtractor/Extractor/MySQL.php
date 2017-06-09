d40ad5aee4ae86097042b03919e9516d0fc80e36
pivnicek <marc@keboola.com>
2017-06-09 19:37:55 +0200

    update to new common reqs + manifest test

--- apps/db-extractor-mysql/src/Keboola/DbExtractor/Extractor/MySQL.php
+++ apps/db-extractor-mysql/src/Keboola/DbExtractor/Extractor/MySQL.php
@@ -7,6 +7,8 @@ namespace Keboola\DbExtractor\Extractor;
 
 use Keboola\DbExtractor\Exception\UserException;
 use Keboola\Temp\Temp;
+use Keboola\Datatype\Definition\GenericStorage;
+use Symfony\Component\Yaml\Yaml;
 
 class MySQL extends Extractor
 {
@@ -97,25 +99,53 @@ class MySQL extends Extractor
 		$this->db->query('SELECT NOW();')->execute();
 	}
 
-    public function listTables()
+    public function getTables(array $tables = null)
     {
-        $tables = [];
-        $res = $this->db->query("SHOW TABLES");
-        while ($table = $res->fetch(\PDO::FETCH_NUM)) {
-            $tables[] = $table[0];
+        $sql = "SELECT * FROM INFORMATION_SCHEMA.TABLES 
+                                  WHERE TABLE_SCHEMA != 'performance_schema' 
+                                  AND TABLE_SCHEMA != 'mysql'
+                                  AND TABLE_SCHEMA != 'information_schema'";
+
+        if (!is_null($tables) && count($tables) > 0) {
+            $sql .= sprintf(
+                " AND TABLE_NAME IN (%s)",
+                implode(',', array_map(function ($table) {
+                    return $this->db->quote($table);
+                }, $tables))
+            );
         }
-        return $tables;
+
+        $res = $this->db->query($sql);
+        $arr = $res->fetchAll(\PDO::FETCH_ASSOC);
+
+        $output = [];
+        foreach ($arr as $table) {
+            $output[] = $this->describeTable($table);
+        }
+        return $output;
     }
 
-    public function describeTable($tableName)
+    protected function describeTable(array $table)
     {
-        $res = $this->db->query(sprintf("SELECT 
-                    COLUMN_NAME, COLUMN_DEFAULT, IS_NULLABLE, DATA_TYPE, 
-                    CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION, NUMERIC_SCALE, COLUMN_KEY
-                    FROM INFORMATION_SCHEMA.COLUMNS
-                    WHERE TABLE_NAME = %s", $this->db->quote($tableName)));
+        $tabledef = [
+            'name' => $table['TABLE_NAME'],
+            'schema' => (isset($table['TABLE_SCHEMA'])) ? $table['TABLE_SCHEMA'] : null,
+            'type' => (isset($table['TABLE_TYPE'])) ? $table['TABLE_TYPE'] : null,
+            'rowCount' => (isset($table['TABLE_ROWS'])) ? $table['TABLE_ROWS'] : null
+        ];
+
+        $sql = sprintf("SELECT c.*, 
+                    CONSTRAINT_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME, REFERENCED_TABLE_SCHEMA
+                    FROM INFORMATION_SCHEMA.COLUMNS as c 
+                    LEFT OUTER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE as kcu
+                    ON c.TABLE_NAME = kcu.TABLE_NAME AND c.COLUMN_NAME = kcu.COLUMN_NAME
+                    WHERE c.TABLE_NAME = %s", $this->db->quote($table['TABLE_NAME']));
+
+        $res = $this->db->query($sql);
         $columns = [];
-        while ($column = $res->fetch(\PDO::FETCH_ASSOC)) {
+
+        $rows = $res->fetchAll(\PDO::FETCH_ASSOC);
+        foreach ($rows as $i => $column) {
             $length = ($column['CHARACTER_MAXIMUM_LENGTH']) ? $column['CHARACTER_MAXIMUM_LENGTH'] : null;
             if (is_null($length) && !is_null($column['NUMERIC_PRECISION'])) {
                 if ($column['NUMERIC_SCALE'] > 0) {
@@ -127,12 +157,71 @@ class MySQL extends Extractor
             $columns[] = [
                 "name" => $column['COLUMN_NAME'],
                 "type" => $column['DATA_TYPE'],
-                "primary" => ($column['COLUMN_KEY'] === "PRI") ? true : false,
+                "primaryKey" => ($column['COLUMN_KEY'] === "PRI") ? true : false,
                 "length" => $length,
                 "nullable" => ($column['IS_NULLABLE'] === "NO") ? false : true,
-                "default" => $column['COLUMN_DEFAULT']
+                "default" => $column['COLUMN_DEFAULT'],
+                "ordinalPosition" => $column['ORDINAL_POSITION']
             ];
+
+            if (!is_null($column['CONSTRAINT_NAME']) ) {
+                $columns[$i]['constraintName'] = $column['CONSTRAINT_NAME'];
+            }
+            if (!is_null($column['REFERENCED_TABLE_NAME'])) {
+                $columns[$i]['foreignKeyRefSchema'] = $column['REFERENCED_TABLE_SCHEMA'];
+                $columns[$i]['foreignKeyRefTable'] = $column['REFERENCED_TABLE_NAME'];
+                $columns[$i]['foreignKeyRefColumn'] = $column['REFERENCED_COLUMN_NAME'];
+            }
         }
-        return $columns;
+        $tabledef['columns'] = $columns;
+        return $tabledef;
+    }
+
+    protected function createManifest($table)
+    {
+        $outFilename = $this->dataDir . '/out/tables/' . $table['outputTable'] . '.csv.manifest';
+
+        $manifestData = [
+            'destination' => $table['outputTable'],
+            'incremental' => $table['incremental']
+        ];
+
+        if (!empty($table['primaryKey'])) {
+            $manifestData['primary_key'] = $table['primaryKey'];
+        }
+
+        if (isset($table['table']) && !is_null($table['table']) && !empty($table['columns'])) {
+            $tableDetails = $this->getTables([$table['table']])[0];
+            $columnMetadata = [];
+            foreach ($tableDetails['columns'] as $column) {
+                if (!in_array($column['name'], $table['columns'])) {
+                    continue;
+                }
+                $datatypeKeys = ['type', 'length', 'nullable', 'default', 'format'];
+                $datatype = new GenericStorage(
+                    $column['type'],
+                    array_intersect_key($column, array_flip($datatypeKeys))
+                );
+                $columnMetadata[$column['name']] = $datatype->toMetadata();
+                $nonDatatypeKeys = array_diff_key($column, array_flip($datatypeKeys));
+                foreach ($nonDatatypeKeys as $key => $value) {
+                    if ($key !== 'name') {
+                        $columnMetadata[$column['name']][] = [
+                            'key' => "KBC." . $key,
+                            'value'=> $value
+                        ];
+                    }
+                }
+            }
+            unset($tableDetails['columns']);
+            foreach ($tableDetails as $key => $value) {
+                $manifestData['metadata'][] = [
+                    "key" => "KBC." . $key,
+                    "value" => $value
+                ];
+            }
+            $manifestData['column_metadata'] = $columnMetadata;
+        }
+        return file_put_contents($outFilename, Yaml::dump($manifestData));
     }
 }
