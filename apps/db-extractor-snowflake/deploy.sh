30f82eb46187f09766ac160eca673f0a2e966e97
pivnicek <marc@keboola.com>
2017-08-02 17:37:24 +0200

    CI: run test job and update dev portal

--- apps/db-extractor-snowflake/deploy.sh
+++ apps/db-extractor-snowflake/deploy.sh
@@ -5,3 +5,23 @@ docker tag keboola/db-extractor-snowflake:latest $REPOSITORY:latest
 eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ex-db-snowflake)
 docker push $REPOSITORY:$TRAVIS_TAG
 docker push $REPOSITORY:latest
+eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola keboola.ex-db-snowflake $TRAVIS_TAG)
+
+export SYRUP_CLI=quay.io/keboola/syrup-cli
+
+docker pull $SYRUP_CLI:latest
+# run simple job
+docker run --rm -e KBC_STORAGE_TOKEN=$KBC_SYRUP_CLI_TOKEN \
+   $SYRUP_CLI:latest run-job keboola.ex-db-snowflake 288409174 $TRAVIS_TAG
+
+if [ $? -ne 0 ]; then
+  echo 'Simple test job run failed'
+  exit 1;
+fi
+
+# If those jobs pass then go ahead and update the DP tag
+docker run --rm \
+  -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME \
+  -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD \
+  -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL \
+  quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola keboola.ex-db-snowflake $TRAVIS_TAG
