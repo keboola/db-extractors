6e703190396b6e84b3a9bb026eac3ab5ef7f6002
pivnicek <marc@keboola.com>
2017-08-08 17:42:03 +0200

    turn on error reporting in deploy

--- apps/db-extractor-mysql/deploy.sh
+++ apps/db-extractor-mysql/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+set -e
+
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ex-db-mysql`
 docker tag keboola/ex-db-mysql:latest $REPOSITORY:$TRAVIS_TAG
