eb9b51d0e4845768f422677d779b7a34fe4367a1
pivnicek <marc@keboola.com>
2017-08-14 14:51:15 +0200

    update getTables

--- apps/db-extractor-oracle/src/Keboola/DbExtractor/Extractor/Oracle.php
+++ apps/db-extractor-oracle/src/Keboola/DbExtractor/Extractor/Oracle.php
@@ -68,7 +68,23 @@ class Oracle extends Extractor
 
 	public function getTables(array $tables = null)
     {
-        $stmt = oci_parse($this->db, "SELECT * FROM all_tables WHERE TABLESPACE_NAME != 'SYSAUX' AND OWNER != 'SYS' ORDER BY TABLE_NAME");
+
+        $sql = "SELECT * FROM all_tables 
+                        WHERE TABLESPACE_NAME != 'SYSAUX' AND TABLESPACE_NAME != 'SYSTEM' 
+                        AND OWNER != 'SYS' AND OWNER != 'SYSTEM'";
+
+        if (!is_null($tables) && count($tables) > 0) {
+            $sql .= sprintf(
+                " AND TABLE_NAME IN ('%s')",
+                implode("','", array_map(function ($table) {
+                    return $table;
+                }, $tables))
+            );
+        }
+        $sql .= $stmt = " ORDER BY TABLE_NAME";
+
+        $stmt = oci_parse($this->db, $sql);
+
         $success = @oci_execute($stmt);
         if (!$success) {
             $error = oci_error($stmt);
@@ -77,7 +93,6 @@ class Oracle extends Extractor
 
         $output = [];
         $numTables = oci_fetch_all($stmt, $tables, 0, -1, OCI_FETCHSTATEMENT_BY_ROW);
-        echo "FOUND $numTables TABLES \n";
         foreach ($tables as $table) {
             $output[] = $this->describeTable($table);
         }
@@ -86,6 +101,12 @@ class Oracle extends Extractor
 
     protected function describeTable(array $table)
     {
+        $tabledef = [
+            'name' => $table['TABLE_NAME'],
+            'schema' => $table['TABLESPACE_NAME'],
+            'owner' => $table['OWNER'],
+            'rowCount' => $table['NUM_ROWS']
+        ];
         $sql = sprintf(
             "SELECT COLS.*, 
             REFCOLS.CONSTRAINT_NAME, REFCOLS.CONSTRAINT_TYPE, REFCOLS.INDEX_NAME, 
@@ -95,9 +116,9 @@ class Oracle extends Extractor
                         AC.R_CONSTRAINT_NAME, AC.INDEX_NAME, AC.CONSTRAINT_TYPE, AC.R_OWNER
                 FROM ALL_CONS_COLUMNS ACC
                 JOIN ALL_CONSTRAINTS AC 
-                ON ACC.CONSTRAINT_NAME = AC.CONSTRAINT_NAME
+                ON ACC.CONSTRAINT_NAME = AC.CONSTRAINT_NAME WHERE AC.CONSTRAINT_TYPE IN ('P', 'U', 'R')
             ) REFCOLS ON COLS.TABLE_NAME = REFCOLS.TABLE_NAME AND COLS.COLUMN_NAME = REFCOLS.COLUMN_NAME
-            WHERE COLS.TABLE_NAME = '%s'",
+            WHERE COLS.TABLE_NAME = '%s' ORDER BY COLS.COLUMN_ID",
             $table['TABLE_NAME']
         );
         $stmt = oci_parse($this->db, $sql);
@@ -105,37 +126,55 @@ class Oracle extends Extractor
         $success = @oci_execute($stmt);
         if (!$success) {
             $error = oci_error($stmt);
-            throw new UserException("Error describing table $tableName: " . $error['message']);
+            throw new UserException("Error describing table {$table['TABLE_NAME']}: " . $error['message']);
         }
 
         $numrows = oci_fetch_all($stmt, $desc, 0, -1, OCI_FETCHSTATEMENT_BY_ROW);
 
         $columns = [];
+        $previousOrdinalPos = 0;
         foreach ($desc as $i => $column) {
             $length = $column['DATA_LENGTH'];
             if (!is_null($column['DATA_PRECISION'])  && !is_null($column['DATA_SCALE'])) {
                 $length = $column['DATA_PRECISION'] . "," . $column['DATA_SCALE'];
             }
-            $columns[$i] = [
-                "name" => $column['COLUMN_NAME'],
-                "type" => $column['DATA_TYPE'],
-                "nullable" => ($column['NULLABLE'] === 'Y') ? true : false,
-                "default" => $column['DATA_DEFAULT'],
-                "length" => $length,
-                "ordinalPosition" => $column['COLUMN_ID'],
-            ];
+
+            if ($column['COLUMN_ID'] > $previousOrdinalPos) {
+                $columns[$column['COLUMN_ID'] - 1] = [
+                    "name" => $column['COLUMN_NAME'],
+                    "type" => $column['DATA_TYPE'],
+                    "nullable" => ($column['NULLABLE'] === 'Y') ? true : false,
+                    "default" => $column['DATA_DEFAULT'],
+                    "length" => $length,
+                    "ordinalPosition" => $column['COLUMN_ID'],
+                    "primaryKey" => false,
+                    "uniqueKey" => false
+                ];
+            }
 
             if (!is_null($column['CONSTRAINT_TYPE'])) {
-                $columns[$i]['indexed'] = true;
-                $columns[$i]['constraintName'] = $column['CONSTRAINT_NAME'];
-                $columns[$i]['primaryKey'] = ($column['CONSTRAINT_TYPE'] === 'P') ? true : false;
-                $columns[$i]['uniqueKey'] = ($column['CONSTRAINT_TYPE'] === 'U') ? true : false;
-                if ($column['CONSTRAINT_TYPE'] === 'R') {
-                    $columns[$i]['foreignKeyRefTable'] = $column['R_OWNER'];
-                    $columns[$i]['foreignKeyRef'] = $column['R_CONSTRAINT_NAME'];
+                switch ($column['CONSTRAINT_TYPE']) {
+                    case 'R':
+                        $columns[$column['COLUMN_ID'] - 1]['foreignKeyName'] = $column['CONSTRAINT_NAME'];
+                        $columns[$column['COLUMN_ID'] - 1]['foreignKeyRefTable'] = $column['R_OWNER'];
+                        $columns[$column['COLUMN_ID'] - 1]['foreignKeyRef'] = $column['R_CONSTRAINT_NAME'];
+                        break;
+                    case 'P':
+                        $columns[$column['COLUMN_ID'] - 1]['primaryKey'] = true;
+                        $columns[$column['COLUMN_ID'] - 1]['primaryKeyName'] = $column['CONSTRAINT_NAME'];
+                        break;
+                    case 'U':
+                        $columns[$column['COLUMN_ID'] - 1]['uniqueKey'] = true;
+                        $columns[$column['COLUMN_ID'] - 1]['uniqueKeyName'] = $column['CONSTRAINT_NAME'];
+                        break;
+                    default:
+                        break;
                 }
             }
+            $previousOrdinalPos = $column['COLUMN_ID'];
         }
-        return $columns;
+        $tabledef['columns'] = $columns;
+
+        return $tabledef;
     }
 }
