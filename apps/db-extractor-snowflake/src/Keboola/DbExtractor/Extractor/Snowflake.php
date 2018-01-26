bf735ca356c9b0bf47a031f138ba9d0ba1c674aa
Martin Halamíček <martin@halamicek.cz>
2018-01-26 16:31:43 +0100

    methods order

--- apps/db-extractor-snowflake/src/Keboola/DbExtractor/Extractor/Snowflake.php
+++ apps/db-extractor-snowflake/src/Keboola/DbExtractor/Extractor/Snowflake.php
@@ -61,50 +61,44 @@ class Snowflake extends Extractor
         return $connection;
     }
 
-    private function quote($value)
+    public function testConnection()
     {
-        return "'" . addslashes($value) . "'";
-    }
+        $this->db->query('SELECT current_date;');
 
-    /**
-     * @param $output
-     * @param $path
-     * @return \SplFileInfo[]
-     * @throws \Exception
-     */
-    private function parseFiles($output, $path)
-    {
-        $files = [];
-        $lines = explode("\n", $output);
-
-        $lines = array_map(
-            function ($item) {
-                $item = trim($item, '|');
-                return array_map('trim', explode('|', $item));
-            },
-            array_filter(
-                $lines,
-                function ($item) {
-                    $item = trim($item);
-                    return preg_match('/^\|.+\|$/ui', $item) && preg_match('/([a-z0-9\_\-\.]+\.gz)/ui', $item);
-                }
-            )
-        );
-
-        foreach ($lines as $line) {
-            if (!preg_match('/^downloaded$/ui', $line[2])) {
-                throw new \Exception(sprintf("Cannot download file: %s Status: %s", $line[0], $line[2]));
-            }
-
-            $file = new \SplFileInfo($path . '/' . $line[0]);
-            if ($file->isFile()) {
-                $files[] = $file;
-            } else {
-                throw new \Exception("Missing file: " . $line[0]);
-            }
+        $defaultWarehouse = $this->getUserDefaultWarehouse();
+        if (!$defaultWarehouse && !$this->warehouse) {
+            throw new UserException('Specify "warehouse" parameter');
         }
 
-        return $files;
+        $warehouse = $defaultWarehouse;
+        if ($this->warehouse) {
+            $warehouse = $this->warehouse;
+        }
+
+        try {
+            $this->db->query(sprintf(
+                'USE WAREHOUSE %s;',
+                $this->db->quoteIdentifier($warehouse)
+            ));
+        } catch (\Exception $e) {
+            if (preg_match('/Object does not exist/ui', $e->getMessage())) {
+                throw new UserException(sprintf('Invalid warehouse "%s" specified', $warehouse));
+            } else {
+                throw $e;
+            }
+        }
+    }
+
+    public function export(array $table)
+    {
+        $outputTable = $table['outputTable'];
+
+        $this->logger->info("Exporting to " . $outputTable);
+
+
+        $this->exportAndDownload($table);
+
+        return $outputTable;
     }
 
     private function exportAndDownload(array $table)
@@ -274,111 +268,6 @@ class Snowflake extends Extractor
         $this->logger->info(sprintf("%d files (%s) downloaded", count($csvFiles), $bytes));
     }
 
-    private function generateStageName()
-    {
-        return '~';
-    }
-
-    /**
-     * @param $dbParams
-     * @return \SplFileInfo
-     */
-    private function crateSnowSqlConfig($dbParams)
-    {
-        $cliConfig[] = '';
-        $cliConfig[] = '[options]';
-        $cliConfig[] = 'exit_on_error = true';
-        $cliConfig[] = '';
-        $cliConfig[] = '[connections.downloader]';
-        $cliConfig[] = sprintf('accountname = "%s"', AccountUrlParser::parse($dbParams['host']));
-        $cliConfig[] = sprintf('username = "%s"', $dbParams['user']);
-        $cliConfig[] = sprintf('password = "%s"', $dbParams['password']);
-        $cliConfig[] = sprintf('dbname = "%s"', $dbParams['database']);
-        $cliConfig[] = sprintf('schemaname = "%s"', $dbParams['schema']);
-
-        if (isset($dbParams['warehouse'])) {
-            $cliConfig[] = sprintf('warehousename = "%s"', $dbParams['warehouse']);
-        }
-
-
-        $file = $this->temp->createFile('snowsql.config');
-        file_put_contents($file, implode("\n", $cliConfig));
-
-        return $file;
-    }
-
-    public function export(array $table)
-    {
-        $outputTable = $table['outputTable'];
-
-        $this->logger->info("Exporting to " . $outputTable);
-
-
-        $this->exportAndDownload($table);
-
-        return $outputTable;
-    }
-
-    private function getUserDefaultWarehouse()
-    {
-        $sql = sprintf(
-            "DESC USER %s;",
-            $this->db->quoteIdentifier($this->user)
-        );
-
-        $config = $this->db->fetchAll($sql);
-
-        foreach ($config as $item) {
-            if ($item['property'] === 'DEFAULT_WAREHOUSE') {
-                return $item['value'] === 'null' ? null : $item['value'];
-            }
-        }
-
-        return null;
-    }
-
-    public function testConnection()
-    {
-        $this->db->query('SELECT current_date;');
-
-        $defaultWarehouse = $this->getUserDefaultWarehouse();
-        if (!$defaultWarehouse && !$this->warehouse) {
-            throw new UserException('Specify "warehouse" parameter');
-        }
-
-        $warehouse = $defaultWarehouse;
-        if ($this->warehouse) {
-            $warehouse = $this->warehouse;
-        }
-
-        try {
-            $this->db->query(sprintf(
-                'USE WAREHOUSE %s;',
-                $this->db->quoteIdentifier($warehouse)
-            ));
-        } catch (\Exception $e) {
-            if (preg_match('/Object does not exist/ui', $e->getMessage())) {
-                throw new UserException(sprintf('Invalid warehouse "%s" specified', $warehouse));
-            } else {
-                throw $e;
-            }
-        }
-    }
-
-    private function execQuery($query)
-    {
-        try {
-            $this->db->query($query);
-        } catch (\Exception $e) {
-            throw new UserException("Query execution error: " . $e->getMessage(), 0, $e);
-        }
-    }
-
-    private function hideCredentialsInQuery($query)
-    {
-        return preg_replace("/(AWS_[A-Z_]*\\s=\\s.)[0-9A-Za-z\\/\\+=]*./", '${1}...\'', $query);
-    }
-
     public function getTables(array $tables = null)
     {
         $sql = "SHOW TABLES";
@@ -466,5 +355,116 @@ class Snowflake extends Extractor
                 $this->db->quoteIdentifier($table['tableName'])
             );
         }
+    }
+
+    /**
+     * @param $output
+     * @param $path
+     * @return \SplFileInfo[]
+     * @throws \Exception
+     */
+    private function parseFiles($output, $path)
+    {
+        $files = [];
+        $lines = explode("\n", $output);
+
+        $lines = array_map(
+            function ($item) {
+                $item = trim($item, '|');
+                return array_map('trim', explode('|', $item));
+            },
+            array_filter(
+                $lines,
+                function ($item) {
+                    $item = trim($item);
+                    return preg_match('/^\|.+\|$/ui', $item) && preg_match('/([a-z0-9\_\-\.]+\.gz)/ui', $item);
+                }
+            )
+        );
+
+        foreach ($lines as $line) {
+            if (!preg_match('/^downloaded$/ui', $line[2])) {
+                throw new \Exception(sprintf("Cannot download file: %s Status: %s", $line[0], $line[2]));
+            }
+
+            $file = new \SplFileInfo($path . '/' . $line[0]);
+            if ($file->isFile()) {
+                $files[] = $file;
+            } else {
+                throw new \Exception("Missing file: " . $line[0]);
+            }
+        }
+
+        return $files;
+    }
+
+    private function quote($value)
+    {
+        return "'" . addslashes($value) . "'";
+    }
+
+    private function generateStageName()
+    {
+        return '~';
+    }
+
+    /**
+     * @param $dbParams
+     * @return \SplFileInfo
+     */
+    private function crateSnowSqlConfig($dbParams)
+    {
+        $cliConfig[] = '';
+        $cliConfig[] = '[options]';
+        $cliConfig[] = 'exit_on_error = true';
+        $cliConfig[] = '';
+        $cliConfig[] = '[connections.downloader]';
+        $cliConfig[] = sprintf('accountname = "%s"', AccountUrlParser::parse($dbParams['host']));
+        $cliConfig[] = sprintf('username = "%s"', $dbParams['user']);
+        $cliConfig[] = sprintf('password = "%s"', $dbParams['password']);
+        $cliConfig[] = sprintf('dbname = "%s"', $dbParams['database']);
+        $cliConfig[] = sprintf('schemaname = "%s"', $dbParams['schema']);
+
+        if (isset($dbParams['warehouse'])) {
+            $cliConfig[] = sprintf('warehousename = "%s"', $dbParams['warehouse']);
+        }
+
+
+        $file = $this->temp->createFile('snowsql.config');
+        file_put_contents($file, implode("\n", $cliConfig));
+
+        return $file;
+    }
+
+    private function getUserDefaultWarehouse()
+    {
+        $sql = sprintf(
+            "DESC USER %s;",
+            $this->db->quoteIdentifier($this->user)
+        );
+
+        $config = $this->db->fetchAll($sql);
+
+        foreach ($config as $item) {
+            if ($item['property'] === 'DEFAULT_WAREHOUSE') {
+                return $item['value'] === 'null' ? null : $item['value'];
+            }
+        }
+
+        return null;
+    }
+
+    private function execQuery($query)
+    {
+        try {
+            $this->db->query($query);
+        } catch (\Exception $e) {
+            throw new UserException("Query execution error: " . $e->getMessage(), 0, $e);
+        }
+    }
+
+    private function hideCredentialsInQuery($query)
+    {
+        return preg_replace("/(AWS_[A-Z_]*\\s=\\s.)[0-9A-Za-z\\/\\+=]*./", '${1}...\'', $query);
     }
 }
