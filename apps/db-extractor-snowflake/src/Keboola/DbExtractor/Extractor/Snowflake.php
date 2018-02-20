2541dda554d77c50c10763ed3d2effafd73a7c10
pivnicek <marc@keboola.com>
2018-02-20 12:53:50 +0100

    fix: limit 0 -> 1 on query hint

--- apps/db-extractor-snowflake/src/Keboola/DbExtractor/Extractor/Snowflake.php
+++ apps/db-extractor-snowflake/src/Keboola/DbExtractor/Extractor/Snowflake.php
@@ -116,7 +116,7 @@ class Snowflake extends Extractor
 
         // Create temporary view from the supplied query
         $sql = sprintf(
-            "SELECT * FROM (%s) LIMIT 0;",
+            "SELECT * FROM (%s) LIMIT 1;",
             rtrim(trim($query), ';')
         );
 
