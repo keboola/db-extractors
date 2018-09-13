6af9c0165457ea30e3d68a45f0e10c4eec58954d
pivnicek <marc@keboola.com>
2018-09-13 16:44:48 +0200

    only create table if it doesn't exist

--- libs/db-extractor-common/tests/RetryTest.php
+++ libs/db-extractor-common/tests/RetryTest.php
@@ -44,36 +44,47 @@ class RetryTest extends ExtractorTest
 
     private function setupLargeTable(string $sourceFileName): void
     {
-        // Set up the data table
-        $csv = new CsvFile($sourceFileName);
-        $header = ["usergender", "usercity", "usersentiment", "zipcode", "sku", "createdat", "category"];
-        $csv->writeRow($header);
-        for ($i = 0; $i < self::ROW_COUNT - 1; $i++) { // -1 for the header
-            $csv->writeRow([uniqid('g'), "The Lakes", "1", "89124", "ZD111402", "2013-09-23 22:38:30", uniqid('c')]);
-        }
-        $createTableSql = sprintf(
-            "CREATE TABLE %s.%s (%s) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;",
-            'odin4test',
-            'sales',
-            implode(
-                ', ',
-                array_map(function ($column) {
-                    return $column . ' text NULL';
-                }, $header)
-            )
+        $res = $this->pdo->query(
+            "SELECT * 
+            FROM information_schema.tables
+            WHERE table_schema = 'odin4test' 
+                AND table_name = 'sales'
+            LIMIT 1;"
         );
-        $this->pdo->exec($createTableSql);
-        $fileName = (string) $csv;
-        $query = "
-			LOAD DATA LOCAL INFILE '{$fileName}'
-			INTO TABLE `odin4test`.`sales`
-			CHARACTER SET utf8
-			FIELDS TERMINATED BY ','
-			OPTIONALLY ENCLOSED BY '\"'
- 			ESCAPED BY ''
- 			IGNORE 1 LINES
- 		";
-        $this->pdo->exec($query);
+        $tableExists = $res->rowCount() > 0;
+
+        if (!$tableExists) {
+            // Set up the data table
+            $csv = new CsvFile($sourceFileName);
+            $header = ["usergender", "usercity", "usersentiment", "zipcode", "sku", "createdat", "category"];
+            $csv->writeRow($header);
+            for ($i = 0; $i < self::ROW_COUNT - 1; $i++) { // -1 for the header
+                $csv->writeRow([uniqid('g'), "The Lakes", "1", "89124", "ZD111402", "2013-09-23 22:38:30", uniqid('c')]);
+            }
+            $createTableSql = sprintf(
+                "CREATE TABLE %s.%s (%s) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;",
+                'odin4test',
+                'sales',
+                implode(
+                    ', ',
+                    array_map(function ($column) {
+                        return $column . ' text NULL';
+                    }, $header)
+                )
+            );
+            $this->pdo->exec($createTableSql);
+            $fileName = (string) $csv;
+            $query = "
+                LOAD DATA LOCAL INFILE '{$fileName}'
+                INTO TABLE `odin4test`.`sales`
+                CHARACTER SET utf8
+                FIELDS TERMINATED BY ','
+                OPTIONALLY ENCLOSED BY '\"'
+                ESCAPED BY ''
+                IGNORE 1 LINES
+            ";
+            $this->pdo->exec($query);
+        }
     }
 
     private function getRetryConfig(): array
