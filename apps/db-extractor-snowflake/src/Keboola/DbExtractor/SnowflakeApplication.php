f05ddfa8c6088248c5e08c921f03849b79c84fb8
pivnicek <marc@keboola.com>
2018-09-26 18:26:43 +0200

    update application constructor

--- apps/db-extractor-snowflake/src/Keboola/DbExtractor/SnowflakeApplication.php
+++ apps/db-extractor-snowflake/src/Keboola/DbExtractor/SnowflakeApplication.php
@@ -5,12 +5,12 @@ use Keboola\DbExtractor\Configuration\SnowflakeConfigDefinition;
 
 class SnowflakeApplication extends Application
 {
-    public function __construct(array $config, $dataDir)
+    public function __construct(array $config, Logger $logger, array $state, string $dataDir)
     {
         $config['parameters']['data_dir'] = $dataDir;
         $config['parameters']['extractor_class'] = 'Snowflake';
 
-        parent::__construct($config);
+        parent::__construct($config, $logger, $state);
 
         $this->setConfigDefinition(new SnowflakeConfigDefinition());
     }
