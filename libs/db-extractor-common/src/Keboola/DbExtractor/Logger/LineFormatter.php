bd88cc09279ff61f049d2bf3e2713b4436a14595
pivnicek <marc@keboola.com>
2018-11-08 16:45:51 +0100

    fix normalize method signature

--- libs/db-extractor-common/src/Keboola/DbExtractor/Logger/LineFormatter.php
+++ libs/db-extractor-common/src/Keboola/DbExtractor/Logger/LineFormatter.php
@@ -12,12 +12,12 @@ class LineFormatter extends \Monolog\Formatter\LineFormatter
      * @param CsvFile|array $data
      * @return array|string
      */
-    protected function normalize($data)
+    protected function normalize($data, $depth = 0)
     {
         if ($data instanceof CsvFile) {
             return "csv file: " . $data->getFilename();
         } else {
-            return parent::normalize($data);
+            return parent::normalize($data, $depth);
         }
     }
 }
