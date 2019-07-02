48d44dd922d89f6cd66e7c1c1dc3722ddffdc18a
Ondra Jodas <ondra.jodas@gmail.com>
2019-07-02 16:43:36 +0200

    remove config validation

--- libs/db-extractor-common/src/Keboola/DbExtractor/Application.php
+++ libs/db-extractor-common/src/Keboola/DbExtractor/Application.php
@@ -8,17 +8,14 @@ use Keboola\DbExtractor\Configuration\ActionConfigRowDefinition;
 use Keboola\DbExtractor\Configuration\ConfigDefinition;
 use Keboola\DbExtractor\Configuration\ConfigRowDefinition;
 use Keboola\DbExtractor\Exception\UserException;
-use Keboola\DbExtractorLogger\Logger;
+use Keboola\DbExtractorConfig\Config;
 use Pimple\Container;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
-use Symfony\Component\Config\Definition\Exception\Exception as ConfigException;
-use Symfony\Component\Config\Definition\Processor;
 use ErrorException;
 
 class Application extends Container
 {
-    /** @var ConfigurationInterface */
-    private $configDefinition;
+    /** @var Config $config */
+    protected $config;
 
     public function __construct(array $config, Logger $logger, array $state = [])
     {
@@ -44,112 +41,26 @@ class Application extends Container
             return $app['extractor_factory']->create($app['logger']);
         };
         if (isset($this['parameters']['tables'])) {
-            $this->configDefinition = new ConfigDefinition();
+            $this->config = new Config(new ConfigDefinition());
         } else {
             if ($this['action'] === 'run') {
-                $this->configDefinition = new ConfigRowDefinition();
+                // @TODO - dodělat do Config repa RowDefinition
+                $this->config = new Config(new ConfigRowDefinition());
             } else {
-                $this->configDefinition = new ActionConfigRowDefinition();
+                // @TODO - dodělat do Config repa RowActionDefinition
+                $this->config = new Config(new ActionConfigRowDefinition());
             }
         }
     }
 
     public function run(): array
     {
-        $this['parameters'] = $this->validateParameters($this['parameters']);
-
         $actionMethod = $this['action'] . 'Action';
         if (!method_exists($this, $actionMethod)) {
             throw new UserException(sprintf('Action "%s" does not exist.', $this['action']));
         }
 
         return $this->$actionMethod();
-    }
-
-    public function setConfigDefinition(ConfigurationInterface $definition): void
-    {
-        $this->configDefinition = $definition;
-    }
-
-    private function isTableValid(array $table): bool
-    {
-        if (!array_key_exists('schema', $table)) {
-            return false;
-        }
-        if (!array_key_exists('tableName', $table)) {
-            return false;
-        }
-        if ($table['tableName'] === '') {
-            return false;
-        }
-        return true;
-    }
-
-    private function validateTableParameters(array $table): void
-    {
-        if (isset($table['query']) && $table['query'] !== '') {
-            if (isset($table['table'])) {
-                throw new ConfigException(
-                    sprintf(
-                        'Invalid Configuration for "%s". Both table and query cannot be set together.',
-                        $table['outputTable']
-                    )
-                );
-            }
-            if (isset($table['incrementalFetchingColumn'])) {
-                throw new ConfigException(
-                    sprintf(
-                        'Invalid Configuration for "%s". Incremental fetching is not supported for advanced queries.',
-                        $table['outputTable']
-                    )
-                );
-            }
-        } else if (!isset($table['table'])) {
-            throw new ConfigException(
-                sprintf(
-                    'Invalid Configuration for "%s". One of table or query is required.',
-                    $table['outputTable']
-                )
-            );
-        } else if (!$this->isTableValid($table['table'])) {
-            throw new ConfigException(
-                sprintf(
-                    'Invalid Configuration for "%s". The table property requires "tableName" and "schema"',
-                    $table['outputTable']
-                )
-            );
-        } else if (isset($table['incrementalFetching']['autoIncrementColumn']) && empty($table['primaryKey'])) {
-            $this['logger']->warn('An import autoIncrement column is being used but output primary key is not set.');
-        }
-    }
-
-    private function validateParameters(array $parameters): array
-    {
-        try {
-            $processor = new Processor();
-            $processedParameters = $processor->processConfiguration(
-                $this->configDefinition,
-                [$parameters]
-            );
-
-            if (!empty($processedParameters['db']['#password'])) {
-                $processedParameters['db']['password'] = $processedParameters['db']['#password'];
-            }
-
-            if ($this['action'] === 'run') {
-                if (isset($processedParameters['tables'])) {
-                    foreach ($processedParameters['tables'] as $table) {
-                        $this->validateTableParameters($table);
-                    }
-                } else {
-                    $this->validateTableParameters($processedParameters);
-                }
-            }
-
-            return $processedParameters;
-        } catch (ConfigException $e) {
-            throw new UserException($e->getMessage(), 0, $e);
-        }
     }
 
     private function runAction(): array
