cc2c9c276ccf5da746e01ecaada27c50881d5a10
Ondra Jodas <ondra.jodas@gmail.com>
2019-08-06 16:41:34 +0200

    reusable some config methods

--- libs/db-extractor-config/src/Configuration/ActionConfigRowDefinition.php
+++ libs/db-extractor-config/src/Configuration/ActionConfigRowDefinition.php
@@ -9,7 +9,7 @@ use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-class ActionConfigRowDefinition implements ConfigurationInterface
+class ActionConfigRowDefinition extends AbstractConfigDefinition
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
@@ -32,63 +32,5 @@ class ActionConfigRowDefinition implements ConfigurationInterface
         // @formatter:on
 
         return $treeBuilder;
-    }
-
-    /**
-     * @return ArrayNodeDefinition|NodeDefinition
-     */
-    public function addSshNode()
-    {
-        $builder = new TreeBuilder();
-        $node = $builder->root('ssh');
-
-        // @formatter:off
-        $node
-            ->children()
-                ->booleanNode('enabled')->end()
-                ->arrayNode('keys')
-                    ->children()
-                        ->scalarNode('private')->end()
-                        ->scalarNode('#private')->end()
-                        ->scalarNode('public')->end()
-                    ->end()
-                ->end()
-                ->scalarNode('sshHost')->end()
-                ->scalarNode('sshPort')->end()
-                ->scalarNode('remoteHost')->end()
-                ->scalarNode('remotePort')->end()
-                ->scalarNode('localPort')->end()
-                ->scalarNode('user')->end()
-            ->end();
-        // @formatter:on
-
-        return $node;
-    }
-
-    public function addDbNode(): NodeDefinition
-    {
-        $builder = new TreeBuilder();
-        $node = $builder->root('db');
-
-        // @formatter:off
-        $node
-            ->children()
-                ->scalarNode('driver')->end()
-                ->scalarNode('host')->end()
-                ->scalarNode('port')->end()
-                ->scalarNode('database')
-                    ->cannotBeEmpty()
-                ->end()
-                ->scalarNode('user')
-                    ->isRequired()
-                ->end()
-                ->scalarNode('#password')
-                    ->isRequired()
-                ->end()
-                ->append($this->addSshNode())
-            ->end();
-        // @formatter:on
-
-        return $node;
     }
 }
--- libs/db-extractor-config/src/Configuration/ConfigDefinition.php
+++ libs/db-extractor-config/src/Configuration/ConfigDefinition.php
@@ -7,9 +7,8 @@ namespace Keboola\DbExtractorConfig\Configuration;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-class ConfigDefinition implements ConfigurationInterface
+class ConfigDefinition extends AbstractConfigDefinition
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
@@ -31,37 +30,6 @@ class ConfigDefinition implements ConfigurationInterface
             ->end();
         // @formatter:on
         return $treeBuilder;
-    }
-
-    /**
-     * @return ArrayNodeDefinition|NodeDefinition
-     */
-    public function addSshNode()
-    {
-        $builder = new TreeBuilder();
-        $node = $builder->root('ssh');
-
-        // @formatter:off
-        $node
-            ->children()
-                ->booleanNode('enabled')->end()
-                ->arrayNode('keys')
-                    ->children()
-                        ->scalarNode('private')->end()
-                        ->scalarNode('#private')->end()
-                        ->scalarNode('public')->end()
-                    ->end()
-                ->end()
-                ->scalarNode('sshHost')->end()
-                ->scalarNode('sshPort')->end()
-                ->scalarNode('remoteHost')->end()
-                ->scalarNode('remotePort')->end()
-                ->scalarNode('localPort')->end()
-                ->scalarNode('user')->end()
-                ->booleanNode('compression')->defaultValue(false)->end()
-            ->end();
-        // @formatter:on
-        return $node;
     }
 
     protected function addTablesNode(): NodeDefinition
@@ -113,66 +81,6 @@ class ConfigDefinition implements ConfigurationInterface
             ->end()
         ;
         // @formatter:on
-        return $node;
-    }
-
-    protected function addValidation(NodeDefinition $definition): NodeDefinition
-    {
-        $definition
-            ->validate()
-            ->ifTrue(function ($v) {
-                if (isset($v['query']) && $v['query'] !== '' && isset($v['table'])) {
-                    return true;
-                }
-                return false;
-            })
-            ->thenInvalid('Both table and query cannot be set together.')
-            ->end()
-            ->validate()
-            ->ifTrue(function ($v) {
-                if (isset($v['query']) && $v['query'] !== '' && isset($v['incrementalFetchingColumn'])) {
-                    return true;
-                }
-                return false;
-            })->thenInvalid('Incremental fetching is not supported for advanced queries.')
-            ->end()
-            ->validate()
-            ->ifTrue(function ($v) {
-                if (!isset($v['table']) && !isset($v['query'])) {
-                    return true;
-                }
-                return false;
-            })->thenInvalid('One of table or query is required')
-            ->end()
-        ;
-
-        return $definition;
-    }
-
-    public function addDbNode(): NodeDefinition
-    {
-        $builder = new TreeBuilder();
-        $node = $builder->root('db');
-
-        // @formatter:off
-        $node
-            ->children()
-                ->scalarNode('driver')->end()
-                ->scalarNode('host')->end()
-                ->scalarNode('port')->end()
-                ->scalarNode('database')
-                    ->cannotBeEmpty()
-                ->end()
-                    ->scalarNode('user')
-                    ->isRequired()
-                ->end()
-                    ->scalarNode('#password')
-                    ->isRequired()
-                ->end()
-                ->append($this->addSshNode())
-            ->end();
-        // @formatter:on
-
         return $node;
     }
 }
