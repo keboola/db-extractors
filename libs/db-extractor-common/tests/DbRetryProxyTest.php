0f6ed239b203ab41e2f5cff070ee1301f0d0c8ea
pivnicek <marc@keboola.com>
2019-09-04 17:28:54 +0200

    simple test for dbRetryProxy

---
+++ libs/db-extractor-common/tests/DbRetryProxyTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\DbExtractor\Tests;
+
+use Keboola\DbExtractor\DbRetryProxy;
+use Keboola\DbExtractorLogger\Logger;
+use Monolog\Handler\TestHandler;
+use PHPUnit\Framework\TestCase;
+
+class DbRetryProxyTest extends TestCase
+{
+    /** @var TestHandler */
+    private $testHandler;
+
+    /** @var Logger */
+    private $logger;
+
+    public function setUp(): void
+    {
+        $this->testHandler = new TestHandler();
+        $this->logger = new Logger('dbRetryProxyTest');
+        $this->logger->pushHandler($this->testHandler);
+    }
+
+    public function testMaxRetries(): void
+    {
+        $retryProxy = new DbRetryProxy(
+            $this->logger
+        );
+
+        $i = 0;
+
+        $res = $retryProxy->call(function () use (&$i): int {
+            $i++;
+            if ($i < 5) {
+                throw new \PDOException('test throw ' . $i);
+            } else {
+                return $i;
+            }
+        });
+        $this->assertEquals(5, $res);
+    }
+}
