bc1e37e9bfa9fe34bae18e0ad38e40447b17a1c0
Ondra Jodas <ondrej.jodas@keboola.com>
2019-11-14 14:13:07 +0100

    supply config row

--- apps/db-extractor-snowflake/src/Keboola/DbExtractor/SnowflakeApplication.php
+++ apps/db-extractor-snowflake/src/Keboola/DbExtractor/SnowflakeApplication.php
@@ -7,7 +7,9 @@ namespace Keboola\DbExtractor;
 use Keboola\DbExtractor\Configuration\NodeDefinition\SnowflakeDbNode;
 use Keboola\DbExtractor\Exception\UserException;
 use Keboola\DbExtractorConfig\Config;
+use Keboola\DbExtractorConfig\Configuration\ActionConfigRowDefinition;
 use Keboola\DbExtractorConfig\Configuration\ConfigDefinition;
+use Keboola\DbExtractorConfig\Configuration\ConfigRowDefinition;
 use Keboola\DbExtractorLogger\Logger;
 use Keboola\DbExtractorConfig\Exception\UserException as ConfigUserException;
 
@@ -23,8 +25,17 @@ class SnowflakeApplication extends Application
 
     protected function buildConfig(array $config): void
     {
+        $dbNode = new SnowflakeDbNode();
         try {
-            $this->config = new Config($config, new ConfigDefinition(new SnowflakeDbNode()));
+            if (isset($config['parameters']['table']) || isset($config['parameters']['query'])) {
+                if ($this['action'] === 'run') {
+                    $this->config = new Config($config, new ConfigRowDefinition($dbNode));
+                } else {
+                    $this->config = new Config($config, new ActionConfigRowDefinition($dbNode));
+                }
+            } else {
+                $this->config = new Config($config, new ConfigDefinition($dbNode));
+            }
         } catch (ConfigUserException $e) {
             throw new UserException($e->getMessage(), $e->getCode(), $e);
         }
