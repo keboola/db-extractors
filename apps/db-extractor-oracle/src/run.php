bbf0d9d20793b51205395e7567ed15803bea5095
Ondra Jodas <ondrej.jodas@keboola.com>
2019-12-27 11:49:02 +0100

    default context for jsonDecode

--- apps/db-extractor-oracle/src/run.php
+++ apps/db-extractor-oracle/src/run.php
@@ -37,9 +37,9 @@ try {
     $inputState = [];
     $inputStateFile = $arguments['data'] . '/in/state.json';
     if (file_exists($inputStateFile)) {
-        $jsonDecode = new JsonDecode(true);
+        $jsonDecode = new JsonDecode([JsonDecode::ASSOCIATIVE => true]);
         $inputState = $jsonDecode->decode(
-            file_get_contents($inputStateFile),
+            (string) file_get_contents($inputStateFile),
             JsonEncoder::FORMAT
         );
     }
