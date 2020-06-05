6a88fc7c05700bdb0cc165d65189ccce61b63f39
Michal Jurečko <michal.jurecko@keboola.com>
2020-06-05 12:02:21 +0200

    Add GetTablesListFilterDefinition

---
+++ libs/db-extractor-config/src/Configuration/GetTablesListFilterDefinition.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\DbExtractorConfig\Configuration;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+class GetTablesListFilterDefinition extends ActionConfigRowDefinition
+{
+    protected function getParametersDefinition(): ArrayNodeDefinition
+    {
+        $parametersNode = parent::getParametersDefinition();
+
+        // @formatter:off
+        $parametersNode
+            ->ignoreExtraKeys(true)
+            ->children()
+                ->arrayNode('tableListFilter')
+                    ->children()
+                        ->booleanNode('listColumns')->defaultTrue()->end()
+                        ->arrayNode('tablesToList')
+                            ->prototype('array')
+                                ->children()
+                                    ->scalarNode('tableName')->end()
+                                    ->scalarNode('schema')->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+        // @formatter:on
+
+        return $parametersNode;
+    }
+}
