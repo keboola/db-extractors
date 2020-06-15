5c328f3123276892f26d5b1d3ac6b9159a82ee21
Ondra Jodas <ondrej.jodas@keboola.com>
2020-06-15 17:29:55 +0200

    added has method for SSLConnectionConfig

--- libs/db-extractor-config/src/Configuration/ValueObject/SSLConnectionConfig.php
+++ libs/db-extractor-config/src/Configuration/ValueObject/SSLConnectionConfig.php
@@ -4,15 +4,17 @@ declare(strict_types=1);
 
 namespace Keboola\DbExtractorConfig\Configuration\ValueObject;
 
+use Keboola\DbExtractorConfig\Exception\PropertyNotSetException;
+
 class SSLConnectionConfig implements ValueObject
 {
-    private ?string $key;
+    private string $key;
 
-    private ?string $cert;
+    private string $cert;
 
-    private ?string $ca;
+    private string $ca;
 
-    private ?string $cipher;
+    private string $cipher;
 
     private bool $verifyServerCert;
 
@@ -37,28 +39,63 @@ class SSLConnectionConfig implements ValueObject
         $this->verifyServerCert = $verifyServerCert;
     }
 
-    public function getKey(): ?string
+    public function hasKey()
     {
+        return $this->key !== null;
+    }
+
+    public function hasCert()
+    {
+        return $this->cert !== null;
+    }
+
+    public function hasCa()
+    {
+        return $this->ca !== null;
+    }
+
+    public function hasCipher()
+    {
+        return $this->cipher !== null;
+    }
+
+    public function getKey(): string
+    {
+        if ($this->key === null) {
+            throw new PropertyNotSetException('Property "key" is not set.');
+        }
         return $this->key;
     }
 
-    public function getCert(): ?string
+    public function getCert(): string
     {
+        if ($this->cert === null) {
+            throw new PropertyNotSetException('Property "cert" is not set.');
+        }
         return $this->cert;
     }
 
-    public function getCa(): ?string
+    public function getCa(): string
     {
+        if ($this->ca === null) {
+            throw new PropertyNotSetException('Property "ca" is not set.');
+        }
         return $this->ca;
     }
 
-    public function getCipher(): ?string
+    public function getCipher(): string
     {
+        if ($this->cipher === null) {
+            throw new PropertyNotSetException('Property "cipher" is not set.');
+        }
         return $this->cipher;
     }
 
     public function isVerifyServerCert(): bool
     {
+        if ($this->verifyServerCert === null) {
+            throw new PropertyNotSetException('Property "verifyServerCert" is not set.');
+        }
         return $this->verifyServerCert;
     }
 }
