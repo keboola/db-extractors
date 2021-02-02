70cbfa36fa030b44baa987bb12e317a76af23710
Ondra Jodas <ondrej.jodas@keboola.com>
2021-02-02 15:22:54 +0100

    tests for check username

---
+++ apps/db-extractor-oracle/tests/phpunit/CheckUsernameTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\DbExtractor\Tests;
+
+use Keboola\DbExtractor\Application;
+use Keboola\DbExtractor\Exception\BadUsernameException;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\Test\TestLogger;
+
+class CheckUsernameTest extends TestCase
+{
+    public function testValidCheckUsername(): void
+    {
+        putenv('KBC_REALUSER=' . (string) getEnv('ORACLE_DB_USER'));
+        new Application($this->createConfig(), new TestLogger());
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testInvalidCheckUsername(): void
+    {
+        putenv('KBC_REALUSER=invalidUsername');
+
+        $this->expectException(BadUsernameException::class);
+        $this->expectExceptionMessage(
+            'Your username "invalidUsername" does not have permission ' .
+            'to run configuration with the database username "tester"'
+        );
+        new Application($this->createConfig(), new TestLogger());
+    }
+
+    public function testTechnicalUsername(): void
+    {
+        putenv('KBC_REALUSER=invalidUsername');
+        $config = $this->createConfig();
+        $config['parameters']['db']['user'] = '_technicalUsername';
+
+        $logger = new TestLogger();
+        new Application($config, $logger);
+
+        Assert::assertTrue(
+            $logger->hasInfoThatContains('Starting export data with a technical username "_technicalUsername".')
+        );
+    }
+
+    public function testDisableChecking(): void
+    {
+        putenv('KBC_REALUSER=invalidUsername');
+        $config = $this->createConfig();
+        $config['image_parameters']['check_kbc_realuser'] = false;
+        new Application($config, new TestLogger());
+
+        $this->expectNotToPerformAssertions();
+    }
+
+    private function createConfig(string $action = 'run'): array
+    {
+        $config = [
+            'parameters' => [
+                'data_dir' => 'testDatadir',
+                'extractor_class' => 'test',
+                'db' => [
+                    'host' => (string) getEnv('ORACLE_DB_HOST'),
+                    'port' => (string) getEnv('ORACLE_DB_PORT'),
+                    'database' => (string) getEnv('ORACLE_DB_DATABASE'),
+                    'user' => (string) getEnv('ORACLE_DB_USER'),
+                    '#password' => (string) getEnv('ORACLE_DB_PASSWORD'),
+                ],
+                'id' => 123,
+                'name' => 'row_name',
+                'table' => [
+                    'tableName' => 'simple',
+                    'schema' => (string) getEnv('ORACLE_DB_DATABASE'),
+                ],
+                'outputTable' => 'output',
+            ],
+            'image_parameters' => [
+                'check_kbc_realuser' => true,
+            ],
+        ];
+
+        if ($action !== 'run') {
+            $config['action'] = $action;
+        }
+
+        return $config;
+    }
+}
