f7efb4f896d0f54f42a0788a8b353176f76d7f8f
Ondra Jodas <ondrej.jodas@keboola.com>
2021-07-27 12:18:13 +0200

    migrate travis -> github actions

--- apps/db-extractor-redshift/deploy.sh
+++ apps/db-extractor-redshift/deploy.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+GITHUB_TAG=${GITHUB_REF/refs\/tags\//}
+
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ex-db-redshift`
-docker tag keboola/ex-db-redshift:latest $REPOSITORY:$TRAVIS_TAG
+docker tag keboola/ex-db-redshift:latest $REPOSITORY:$GITHUB_TAG
 docker tag keboola/ex-db-redshift:latest $REPOSITORY:latest
 eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ex-db-redshift)
-docker push $REPOSITORY:$TRAVIS_TAG
+docker push $REPOSITORY:$GITHUB_TAG
 docker push $REPOSITORY:latest
 
 docker run --rm \
   -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME \
   -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD \
-  quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola keboola.ex-db-redshift $TRAVIS_TAG
+  quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola keboola.ex-db-redshift $GITHUB_TAG
