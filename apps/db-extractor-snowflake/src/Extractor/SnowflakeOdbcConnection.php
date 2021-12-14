74067a4af33d027f96ece4d290170663b692274b
Ondra Jodas <ondrej.jodas@keboola.com>
2021-12-14 13:06:27 +0100

    update keboola/db-extractor-adapter package

--- apps/db-extractor-snowflake/src/Extractor/SnowflakeOdbcConnection.php
+++ apps/db-extractor-snowflake/src/Extractor/SnowflakeOdbcConnection.php
@@ -12,35 +12,16 @@ class SnowflakeOdbcConnection extends OdbcConnection
 {
     use QuoteTrait;
 
-    protected function connect(): void
-    {
-        $this->logger->info(sprintf('Creating ODBC connection to "%s".', $this->dsn));
-        ini_set('odbc.default_cursortype', (string) $this->odbcCursorType);
-        try {
-            /** @var resource|false $connection */
-            $connection = @odbc_connect($this->dsn, $this->user, $this->password, $this->odbcCursorMode);
-        } catch (Throwable $e) {
-            throw new OdbcException($e->getMessage(), $e->getCode(), $e);
-        }
-
-        // "odbc_connect" can generate warning, if "set_error_handler" is not set, so we are checking it manually
-        if ($connection === false) {
-            if (strpos(odbc_errormsg(), 'msg=')) {
-                preg_match('/msg=\'(.*)\'\./', odbc_errormsg(), $message);
-                throw new OdbcException($message[1] . ' ' . odbc_error());
-            }
-            throw new OdbcException(odbc_errormsg() . ' ' . odbc_error());
-        }
-
-        if ($this->init) {
-            ($this->init)($connection);
-        }
-
-        $this->connection = $connection;
-    }
-
     public function testConnection(): void
     {
         $this->query('SELECT current_date;');
+    }
+
+    protected function handleConnectionError(string $error, int $code = 0, ?Throwable $previousException = null): void
+    {
+        if (strpos(odbc_errormsg(), 'msg=')) {
+            preg_match('/msg=\'(.*)\'\./', odbc_errormsg(), $message);
+            throw new OdbcException($message[1] . ' ' . odbc_error());
+        }
     }
 }
