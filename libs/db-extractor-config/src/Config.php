6d77dfc5afbe306feb70b51ecbaa5e5c081a8720
Ondra Jodas <ondrej.jodas@keboola.com>
2022-08-20 08:03:11 +0200

    Config based on BaseConfig from php-component

--- libs/db-extractor-config/src/Config.php
+++ libs/db-extractor-config/src/Config.php
@@ -4,50 +4,22 @@ declare(strict_types=1);
 
 namespace Keboola\DbExtractorConfig;
 
-use Keboola\Component\Config\BaseConfigDefinition;
+use Keboola\Component\Config\BaseConfig;
 use Keboola\DbExtractorConfig\Exception\UserException;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
-use Symfony\Component\Config\Definition\Processor;
 
-class Config
+class Config extends BaseConfig
 {
-    protected array $config;
-
-    private ConfigurationInterface $configDefinition;
-
-    public function __construct(
-        array $config,
-        ?ConfigurationInterface $configDefinition = null
-    ) {
-        $this->setConfigDefinition($configDefinition);
-        $this->setConfig($config);
-    }
 
     /**
      * @param mixed[] $config
      */
-    private function setConfig(array $config): void
+    protected function setConfig(array $config): void
     {
         try {
-            $processor = new Processor();
-            $processedConfig = $processor->processConfiguration($this->configDefinition, [$config]);
-            $this->config = $processedConfig;
+            parent::setConfig($config);
         } catch (InvalidConfigurationException $e) {
             throw new UserException($e->getMessage(), 0, $e);
         }
-    }
-
-    private function setConfigDefinition(?ConfigurationInterface $configDefinition): void
-    {
-        if ($configDefinition === null) {
-            $configDefinition = new BaseConfigDefinition();
-        }
-        $this->configDefinition = $configDefinition;
-    }
-
-    public function getData(): array
-    {
-        return $this->config;
     }
 }
